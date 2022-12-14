2d3c47333951c06cb73481dc9c92a944eb851603
FrozenFL <FROZENF0RLIFE44@gmail.com>
2022-12-14 06:08:14 -0800

    Update 1

---
+++ src/main/java/net/frozenfl/fooditems/FoodItems.java
@@ -0,0 +1,15 @@
+package net.frozenfl.fooditems;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FoodItems implements ModInitializer {
+	public static final String MOD_ID = "fooditems";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/frozenfl/fooditems/FoodItemsClient.java
@@ -0,0 +1,10 @@
+package net.frozenfl.fooditems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FoodItemsClient  implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
